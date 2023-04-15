a5366597a239aa3d17bd589d5ad2e5ba40a371f2
ohhonggi <33611439+ohhonggi@users.noreply.github.com>
2023-04-15 16:25:22 +0900

    [Silver III] Title: 전공책, Time: 128 ms, Memory: 16504 KB -BaekjoonHub

--- 백준/Silver/16508. 전공책/전공책.java
+++ 백준/Silver/16508. 전공책/전공책.java
@@ -14,57 +14,59 @@ public class Main {
      * 2. n번째 글자까지 탐색하여, 모든 글자가 포함되어 있는 경우 가격 계산
      *
      */
-    static boolean[] visited;
-    static int[][] bookCharCount;
+    static int[] wordChars;
+    static int[][] bookChars;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         word = br.readLine();
         n = Integer.parseInt(br.readLine());
         prices = new int[n];
         wordL = word.length();
-        visited = new boolean[n];
-        bookCharCount = new int[n][26];
+        wordChars = new int[26];
+        bookChars = new int[n][26];
 
+        for (char c : word.toCharArray()) {
+            wordChars[c-'A']++;
+        }
+        
         for (int i = 0; i < n; i++) {
             String[] split = br.readLine().split(" ");
             prices[i] = Integer.parseInt(split[0]);
             for (char c : split[1].toCharArray()) {
-                bookCharCount[i][c-'A']++;
+                bookChars[i][c-'A']++;
             }
         }
 
-        dfs(0,0,0);
-        if (min == Integer.MAX_VALUE) {
-            System.out.println(-1);
-        } else {
-            System.out.println(min);
-        }
+        dfs(-1,0,0);
+        System.out.println( min == Integer.MAX_VALUE ? -1 : min);
     }
 
-    public static int dfs(int depth, int price, int count){
-        if (depth == wordL){
-            if (count == depth)
-                return price;
-            else
-                return Integer.MAX_VALUE;
+    public static void dfs(int depth, int price, int count){
+        if (count == wordL){
+            min = Math.min(min, price);
+            return;
         }
+        if (depth == n)
+            return;
 
-        int c = word.charAt(depth) - 'A';
-        for (int i = 0; i < n; i++) {
-            if (bookCharCount[i][c] > 0){
-                bookCharCount[i][c]--;
-                if (visited[i]) {
-                    min = Math.min(min, dfs(depth+1,price, count+1));
-                } else{
-                    visited[i] = true;
-                    min = Math.min(min, dfs(depth+1,price + prices[i], count+1));
-                    visited[i] = false;
+        for (int i = depth+1; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < 26; j++) {
+                if (wordChars[j] > 0 && bookChars[i][j] > 0){
+                    if (wordChars[j] >= bookChars[i][j]){
+                        cnt += bookChars[i][j];
+                    } else {
+                        cnt += wordChars[j];
+                    }
                 }
-                bookCharCount[i][c]++;
+                wordChars[j] -= bookChars[i][j];
+            }
+            dfs(i, price + prices[i], count + cnt);
+            for (int j = 0; j<26; j++){
+                wordChars[j] += bookChars[i][j];
             }
         }
-
-        return min;
     }
 }
