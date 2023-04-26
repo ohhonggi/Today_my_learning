759ff41b2c3be5239a6274b93194769a38d3dbcc
ohhonggi <33611439+ohhonggi@users.noreply.github.com>
2023-04-26 16:18:41 +0900

    [level 2] Title: 소수 찾기, Time: 11.08 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,58 +1,71 @@
 import java.util.HashSet;
 
 class Solution {
-    boolean[] visited;
-    String[] split;
-    int numL;
-    int[] isPrime;
+    // boolean[] visited;
+    // String[] split;
+    // int numL;
     HashSet<Integer> pnums = new HashSet<Integer>();
     
     public int solution(String numbers) {
-        split = numbers.split("");
-        numL = numbers.length();
-        visited = new boolean[numL];
-        isPrime = new int[(int)Math.pow(10,(numL+1))];
-        isPrime[0] = isPrime[1] = -1;
-
-        for (int i = 0; i< split.length; i++){
-            dfs(0, i, split[i]);
+        // split = numbers.split("");
+        // numL = numbers.length();
+        // visited = new boolean[numL];
+        int result = 0;
+        permutation("", numbers);
+        
+        while (pnums.iterator().hasNext()){
+            int num = pnums.iterator().next();
+            pnums.remove(num);
+            if (num == 2)
+                result++;
+            else if (num%2 != 0 && isPrimeNumber(num)){
+                result++;
+            }
         }
-        return pnums.size();
+        return result;
     }
     
-    public void dfs(int depth, int index, String number){
-        if (depth == numL){
-            System.out.println(number);
-            return;
+    public void permutation(String prefix, String numS){
+        int n = numS.length();
+        
+        if (!prefix.equals("")){
+            pnums.add(Integer.parseInt(prefix));
         }
-        else{
-            if (!pnums.contains(Integer.parseInt(number)) 
-                && isPrimeNumber(Integer.parseInt(number))){
-                pnums.add(Integer.parseInt(number));
-            }
-
-            visited[index] = true;
-            for (int i = 0; i< numL; i++){
-                if (!visited[i])
-                    dfs(depth+1, i, number + split[i]);
-            }
-            visited[index] = false;
+        
+        for (int i = 0; i<n; i++){
+            permutation(prefix + numS.charAt(i), numS.substring(0, i) + numS.substring(i+1, n));
         }
     }
     
-    public boolean isPrimeNumber(int number){
-        if (isPrime[number] < 0)
+//     public void dfs(int depth, int index, String number){
+//         if (depth == numL){
+//             System.out.println(number);
+//             return;
+//         }
+//         else{
+//             if (!pnums.contains(Integer.parseInt(number)) 
+//                 && isPrimeNumber(Integer.parseInt(number))){
+//                 pnums.add(Integer.parseInt(number));
+//             }
+
+//             visited[index] = true;
+//             for (int i = 0; i< numL; i++){
+//                 if (!visited[i])
+//                     dfs(depth+1, i, number + split[i]);
+//             }
+//             visited[index] = false;
+//         }
+//     }
+    
+    public boolean isPrimeNumber(int n){
+        if (n == 0 || n == 1){
             return false;
-        else if (isPrime[number] == 1){
-            return true;
         } else {
-            for (int i = 2; i<=(int) Math.sqrt(number); i++){
-                if (number % i == 0){
-                    isPrime[number] = -1;
+            for (int i = 3; i<= (int) Math.sqrt(n); i+=2){
+                if (n % i == 0){
                     return false;
                 }
             }
-            isPrime[number] = 1;
             return true;
         }
     }
