5513afbc99238597df31455d24727e7ca00d156f
ohhonggi <33611439+ohhonggi@users.noreply.github.com>
2023-06-12 14:31:48 +0900

    [level 2] Title: 주식가격, Time: 23.11 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,61 +1,33 @@
 import java.util.*;
 class Solution {
-    List<int[]> stock;
     public int[] solution(int[] prices) {
         /*
             주식의 가격이 같거나 상승 기간 계산
-            
             prices의 길이 100,000 -> 최대 nlogn
-            
-            queue에 저장되는 원소 -> 인덱스, 주식가격            
+            stack에 저장되는 원소 -> 인덱스, 주식가격            
         */
         int pricesL = prices.length;
         int[] answer = new int[pricesL];
         
-        stock = new ArrayList<int[]>(pricesL);
+        Stack<Integer> stock = new Stack<Integer>();
         
         for (int i = 0; i<pricesL; i++){
-            for (int j = 0; j<stock.size(); j++){
-                int[] s = stock.get(j);
-                if ( s[1] > prices[i] ){
-                    answer[s[0]] = i-s[0];
-                    stock.remove(j--);
-                }
+            
+            // stack의 저장된 원소 중, 가격이 떨어진 주식을 찾아 기록 및 제거
+            
+            while (!stock.isEmpty() && prices[i] < prices[stock.peek()]){
+                int sI = stock.pop();
+                answer[sI] = i - sI;
             }
-            stock.add(new int[]{i, prices[i]});
+            stock.push(i);
         }
         
-        for (int j = 0; j<stock.size(); j++){
-            int[] s = stock.get(j);
-            answer[s[0]] = pricesL - 1 - s[0];
+        // queue에 남은 원소 -> 끝까지 가격이 떨어지지 않은 주식들
+        while (!stock.isEmpty()){
+            int sI = stock.pop();
+            answer[sI] = pricesL - 1 - sI;
         }
         
         return answer;
     }
-    
-    // 정확도 O, 시간초과..
-//     public int solution1(int[] prices){
-//         int pricesL = prices.length;
-//         int[] answer = new int[pricesL];
-        
-//         stock = new ArrayList<Integer>(pricesL);
-//         int removedStockCount = 0;
-        
-//         for (int i = 0; i<pricesL; i++){
-            
-//             for (int j = 0; j<i; j++){
-//                 if (answer[j] == 0 && stock.get(j) > prices[i])
-//                     answer[j] = i-j;
-//             }
-//             stock.add(prices[i]);
-//         }
-        
-//         for (int i = 0; i<pricesL; i++){
-//             if (answer[i] == 0)
-//                 answer[i] = pricesL -1 - i;
-//         }
-        
-//         return answer;
-        
-//     }
 }
