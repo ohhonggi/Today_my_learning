9f7e9d44374566bddf90bf25dd4c7461974ef479
ohhonggi <33611439+ohhonggi@users.noreply.github.com>
2023-07-06 22:04:35 +0900

    [level 4] Title: [카카오 인턴] 동굴 탐험, Time: 776.55 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/lv4/67260. ［카카오 인턴］ 동굴 탐험/［카카오 인턴］ 동굴 탐험.java
+++ 프로그래머스/lv4/67260. ［카카오 인턴］ 동굴 탐험/［카카오 인턴］ 동굴 탐험.java
@@ -13,13 +13,16 @@ class Solution {
         - 임의의 서로 다른 두 방 사이의 최단경로는 딱 한 가지 -> cycle이 존재하지 않음
         
         1. 각 방마다 연결 가능한 통로 정보, order 정보를 정리하여 저장
-        2. 0번방을 queue에 삽입 후 탐사 진행
+        2. 0번방을 queue에 삽입
         3. queue의 head를 제거하여 연결된 방을 탐색
-            1) 해당 방이 order에 포함된 방인 경우([A,B]중 A에 속할 때),
-            두 방을 연결 짓는 최초 부모를 찾아 queue에 삽입
-            2) 해당 방이 order에 포함되지 않는 방(전제조건 x)인 경우, 방문 표시 및 queue 삽입
+            1) 해당 방이 order에 포함된 방인 경우([A,B]중 B에 속할 때) 
+              -> A를 방문할 때 까지 기다리도록 Set에 저장
+            2) 해당 방이 order에 포함된 방인 경우([A,B]중 A에 속할 때)
+              ① Set에서 B를 삭제 및 queue에 삽입
+              ② 자신의 depth보다 높은 방들을 queue에 삽입
+            3) 해당 방이 order에 포함되지 않는 방(전제조건 x)인 경우
+              -> 자신의 depth보다 높은 방들을 queue에 삽입
         4. queue가 빌 때 까지 3번 반복
-
     */
     LinkedList<Integer>[] paths;
     int[] orderInfo;
@@ -28,7 +31,7 @@ class Solution {
     HashSet<Integer> waitRoom;
     int[] degree;
     public boolean solution(int n, int[][] path, int[][] order) {
-        paths = new LinkedList[n];
+        paths = new LinkedList[n];  
         queue = new LinkedList();
         waitRoom = new HashSet();
         orderInfo = new int[n];
@@ -46,24 +49,22 @@ class Solution {
             paths[p[1]].add(p[0]);
         }
         
+        // 진입차수 정리
         sortDegree();
         
         // order 정보 정리
         for (int[] o : order){
             orderInfo[o[1]] = o[0];
             findOrder.put(o[0], o[1]);
-            
-            // 0번방을 탐험하기 위해 다른 방을 방문해야하는 조건이 존재하는 경우 -> 탐험 불가
-            if (o[1] == 0)
-                return false;
         }
         
         exploration(n);
         
+        // order가 모두 삭제된 경우 -> 모든 방을 탐색한 경우.
         return findOrder.isEmpty();
     }
     
-    // 진입차수 정리 (0번 방이 root이므로, 0번 방을 기준으로 depth당 +1)
+    // 진입차수 정리 (0번 방을 root로 하고, tree의 depth로 이용)
     public void sortDegree(){
         queue.add(0);
         
@@ -72,6 +73,7 @@ class Solution {
             LinkedList<Integer> path = paths[i];
             
             for (int p : path){
+                // 0번 방이 아니거나 아직 차수가 정리되지 않은 방 -> 방 i보다 depth가 깊은 방
                 if (p != 0 && degree[p] == 0){
                     degree[p] = degree[i] + 1;
                     queue.add(p);
@@ -87,21 +89,20 @@ class Solution {
         while (!queue.isEmpty()){
             int room = queue.remove();
             
-            // 먼저 방문해야할 방이 없는 경우
+            // 먼저 방문해야할 방 조건이 없는 경우
             if (orderInfo[room] == 0){
                 Integer o = findOrder.remove(room);
                 
                 // 해당 방을 방문함으로써 방문할 수 있는 방이 생기는 경우
                 if (o != null){ 
                     orderInfo[o] = 0;
+                    // 방문하지 못했던 방이 존재할 때 -> queue 삽입
                     if (waitRoom.remove(o)){
                         queue.add(o);
                     }
-                    // int commonPath = findCommonPath(o, room);
-                    // if (commonPath >= 0)
-                    //     queue.add(commonPath);
                 }
 
+                // 자신의 depth보다 높은 방들을 queue에 삽입
                 LinkedList<Integer> rooms = paths[room];
                 for (int r : rooms){
                     if (degree[room] < degree[r]){
@@ -109,37 +110,9 @@ class Solution {
                     }
                 }
             } else {
+                // 먼저 방문해야할 방을 방문할 때 까지 대기
                 waitRoom.add(room);
             }
         }
-    }
-    
-    // 4. 두 방을 연결 짓는 방을 찾아 반환
-    public int findCommonPath(int p1, int p2){
-        int p1C = p1;
-        while (p1 != p2) {
-            if (orderInfo[p1] != 0){
-                return -1;
-            }
-            if (degree[p1] < degree[p2]){
-                LinkedList<Integer> path = paths[p2];
-                for (int p : path){
-                    if (degree[p2] > degree[p]){
-                        p2 = p;
-                        break;
-                    }
-                }
-            } else {
-                LinkedList<Integer> path = paths[p1];
-                for (int p : path){
-                    if (degree[p1] > degree[p]){
-                        p1C = p1;
-                        p1 = p;
-                        break;
-                    }
-                }
-            }
-        }
-        return p1C;
     }
 }
