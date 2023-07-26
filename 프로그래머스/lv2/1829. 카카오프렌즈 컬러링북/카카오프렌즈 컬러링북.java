a65df92987906276fa8bdfaf62112c5a41b49623
ohhonggi <33611439+ohhonggi@users.noreply.github.com>
2023-07-26 21:09:40 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 10.38 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -2,38 +2,33 @@ import java.util.HashSet;
 
 class Solution {
     
-    int[][] xy;
+    /*
+        사람들의 질문사항을 보니 이 문제는 체점 관련하여 전역변수를 solution 안에서
+        모두 초기화해야 한다는 에로사항이 있네요..!
+    */
+
+    int[][] xy = new int[][]{{0,-1}, {0,1}, {-1,0}, {1,0}};;
     int rowL, colL, max;
     boolean[][] visited;
     int[][] p;
-    int[] answer;
     public int[] solution(int m, int n, int[][] picture) {
-        xy = new int[][]{{0,-1}, {0,1}, {-1,0}, {1,0}};
         rowL = m;
         colL = n;
         max = 0;
         visited = new boolean[m][n];
         p = picture;
-        answer = new int[2];
-        
-        // for (int i = 0; i<rowL; i++){
-        //     for (int j = 0; j<colL; j++){
-        //         p[i][j] = picture[i][j];
-        //     }
-        // }
-        
-        
+        int areaCount = 0;
+                
         for (int i = 0; i<rowL; i++){
             for (int j = 0; j<colL; j++){
                 if (!visited[i][j] && p[i][j] > 0){
                     max = Math.max(dfs(i,j,p[i][j]), max);
-                    answer[0]++;
+                    areaCount++;
                 }
             }
         }
                 
-        answer[1] = max;
-        return answer;
+        return new int[]{areaCount, max};
     }
     
     public int dfs(int m, int n, int type){
