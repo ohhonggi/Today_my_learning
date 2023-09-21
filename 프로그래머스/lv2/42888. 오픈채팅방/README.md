79056015b3111ea5b573e170fce2345018ee63ac
ohhonggi <33611439+ohhonggi@users.noreply.github.com>
2023-09-22 01:52:27 +0900

    [level 2] Title: 오픈채팅방, Time: 129.99 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/README.md
+++ 프로그래머스/lv2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888#) 
 
 ### 성능 요약
 
-메모리: 144 MB, 시간: 85.01 ms
+메모리: 149 MB, 시간: 129.99 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,19 +1,13 @@
 import java.util.HashMap;
 import java.util.LinkedList;
 class Solution {
-    /*
-        1. 전체 기록에 대한 아이디별 닉네임 총 결과 저장
-        2. 전체 기록을 다시 순환하며, 기록결과 출력
-        
-    */
-    
     HashMap<String, String> nickname;
     public String[] solution(String[] record) {
         LinkedList<String> print = new LinkedList();
         nickname = new HashMap(record.length);
         for (String r : record){
             String[] input = r.split(" ");
-            if (!input[0].equals("Leave")){
+            if (input[0].charAt(0) != 'L'){
                 nickname.put(input[1], input[2]);
             }
         }
@@ -21,9 +15,9 @@ class Solution {
         for (String r : record){
             String[] input = r.split(" ");
             String name = nickname.get(input[1]);
-            if (input[0].equals("Enter")){
+            if (input[0].charAt(0) == 'E'){
                 print.add(join(name));
-            } else if (input[0].equals("Leave")){
+            } else if (input[0].charAt(0) == 'L'){
                 print.add(exit(name));
             }
         }
